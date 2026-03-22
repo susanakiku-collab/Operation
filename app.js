7c2805809ac6ee7a92f2b81f1dc1aa6cd854d2dd
susanakiku-collab <susana.kiku@gmail.com>
2026-03-22 09:33:21 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
-document.getElementById("helloBtn").addEventListener("click", () => {
-  alert("WEBページが動きました。");
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+    const target = document.querySelector(this.getAttribute('href'));
+    target.scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
 });
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show');
+    }
+  });
+});
+
+document.querySelectorAll('.fade').forEach(el => observer.observe(el));
